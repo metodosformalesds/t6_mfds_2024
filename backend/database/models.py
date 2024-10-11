f2ac6d781694e68229251993720d7e3961b2e010
Adrian Rivas <adrian_rvzz@hotmail.com>
2024-10-10 21:05:37 -0600

    Model and serializer for CreditHistory add

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+
+#Credit History Model
+class CreditHistory(models.Model):
+    id_check = models.AutoField(primary_key=True)  # Clave única
+    
+    #Agregar cuando se complete el modelo de Borrower
+    #borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
+    
+    date_account_open = models.DateField()          # Fecha de apertura de la cuenta
+    actual_balance = models.DecimalField(max_digits=10, decimal_places=2)  # Saldo pendiente
+    max_credit = models.DecimalField(max_digits=10, decimal_places=2)      # Cantidad máxima solicitada
+    lim_credit = models.DecimalField(max_digits=10, decimal_places=2)       # Monto máximo disponible
+    pay_history = models.CharField(max_length=50)  # Historial de pagos
+    current_pay_status = models.CharField(max_length=10)  # Estado actual de los pagos
+    accounts_open = models.IntegerField()           # Cuentas activas
+    accounts_closed = models.IntegerField()         # Cuentas cerradas
+    account_fixed_payment = models.BooleanField()   # Cuentas con pago fijo
+    num_mop1 = models.IntegerField()                # Pagos a tiempo
+    num_mop2 = models.IntegerField()                # Retrasos de 1 a 29 días
+    num_mop3 = models.IntegerField()                # Retrasos de 30 a 59 días
+    num_mop4 = models.IntegerField()                # Retrasos de 60 a 89 días
+    num_mop5 = models.IntegerField()                # Retrasos de 90 a 119 días
+    num_mop6 = models.IntegerField()                # Retrasos de 120 a 149 días
+    num_mop7 = models.IntegerField()                # Retrasos de 150 a 179 días
+    check_date = models.DateField()                 # Fecha de consulta
+    code_score = models.FloatField()              # Score crediticio
+    place_of_work = models.CharField(max_length=100)  # Lugar de trabajo
+    salary = models.DecimalField(max_digits=10, decimal_places=2)  # Salario
--- backend/database/views.py
+++ backend/database/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.response import Response
+from database.models import CreditHistory
+from database.serializers import CreditHistorySerializer
 # Create your views here.
+
+#Vista super simple para probar el frontend
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({'message': '¡Hola desde el backend!'}, status=200)
+
+
+#Vista de modelo para credit history
+class CreditHistoryViewSet(viewsets.ModelViewSet):
+    queryset = CreditHistory.objects.all()
+    serializer_class = CreditHistorySerializer
+
+    def list(self, request, *args, **kwargs):
+        return Response({'message': '¡Hola desde el backend!'})
