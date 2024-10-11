b74487e48f826259e68870d2ceca8dec899ba0e9
Adrian Rivas <adrian_rvzz@hotmail.com>
2024-10-10 21:04:41 -0600

    Cors configuration for frontend , .env variables and swagger for api view and testing

--- backend/llamascoin/urls.py
+++ backend/llamascoin/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Endpoint para ver los endpoints
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), 
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
