8c0a7fe85f092a3d815deb0cfb00750a0d294080
BrayanOmarTobiasOrnelas_222773 <al222773@alumnos.uacj.mx>
2024-10-12 13:36:02 -0600

    Actualizacion tabla Borrower y creacion tabla invoiceHistory

--- backend/database/models.py
+++ backend/database/models.py
@@ -10,6 +10,7 @@ class Borrower(models.model):
     Agregar al finalizar modelo loan:
     loan = models.ForeignKey(Loans, on_delete=models.CASCADE)  #relacion con la entidad loan
     '''
+    InvoiceHistory = models.ForeignKey(InvoiceHistory, on_delete=models.CASCADE) #Relacion con la tabla del historial de facturas
     Username_borrower = models.CharField(max_length=50) #Nombre de usuario del prestatario
     Name_borrower = models.CharField() #Nombre completo del prestatario
     Firstname_borrower = models.CharField() #Primer nombre del prestatario
@@ -20,6 +21,7 @@ class Borrower(models.model):
     Password_borrower = models.CharField(max_length=60) #Contraseña de la cuenta del prestatario
     Phonenumber_borrower = models.IntegerField(max_length=10) #Numero de telefono del prestatario
     RFC_borrower = models.CharField(max_length=13) #Registro Federal de Contribuyentes (RFC) relacionado al prestatario
+    CIEC_borrower = models.CharField() #Clave de Identificación Electrónica Confidencial del prestatario
     Fulladdress_borrower = models.CharField() #Direccion completa del prestatario
     City_borrower = models.CharField() #Ciudad del prestatario
     neighboorn_borrower = models.CharField() #Vecindario, barrio, colonia o fraccionamiento del prestatario
@@ -64,11 +66,28 @@ class CreditHistory(models.Model):
     place_of_work = models.CharField(max_length=100)  # Lugar de trabajo
     salary = models.DecimalField(max_digits=10, decimal_places=2)  # Salario
 
+#Invoice Model
+class InvoiceHistory(models.Model):
+    ID_invoice = models.AutoField(primary_key=True)
+    UUID = models.CharField()
+    Download_date = models.DateField()
+    CFDI_date = models.DateField()
+    Timbre_date = models.DateField()
+    Cancellation_date = models.DateField()
+    Status_date = models.DateField()
+    Issuing_party = models.CharField()
+    Receiving_party = models.CharField()
+    Type = models.CharField()
+    Status = models.CharField()
+    Total = models.IntegerField()
+    Acuse = models.BooleanField()
+    Invoice_pay = models.BooleanField()
+
 #Request model
 class UserRequest(models.Model):
     id_request = models.AutoField(primary_key=True) #llave unica de la entidad
     Usercheck = models.ForeignKey(CreditHistory, on_delete=models.CASCADE) # llave foranea de la entidad CreditHistory,
-    borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
+    Borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
     moneylender = models.ForeignKey(Moneylender, on_delate=models.CASCADE) #relacion con el prestamista
     
     #estos atributos aun no estan creados
