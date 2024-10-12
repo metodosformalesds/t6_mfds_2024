d676ea1ccb51e758824be62395f51b9be05033e8
BrayanOmarTobiasOrnelas_222773 <al222773@alumnos.uacj.mx>
2024-10-11 18:36:58 -0600

    Creacion de tablas Borrower y Moneylender

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,18 +2,47 @@ from django.db import models
 
 # Create your models here.
 
+#Borrower Model
+class Borrower(models.model):
+    id_borrower = models.CharField(primary_key=True) #Clave unica del usuario prestatario
+    
+    '''
+    Agregar al finalizar modelo loan:
+    loan = models.ForeignKey(Loans, on_delete=models.CASCADE)  #relacion con la entidad loan
+    '''
+    Username_borrower = models.CharField(max_length=50) #Nombre de usuario del prestatario
+    Name_borrower = models.CharField() #Nombre completo del prestatario
+    Firstname_borrower = models.CharField() #Primer nombre del prestatario
+    Middlename_borrower = models.CharField() #Segundo nombre del prestatario
+    Firstsurname_borrower = models.CharField() #Primer apellido o apellido paterno del prestatario
+    Secondsurname_borrower = models.CharField() #Segundo apellido o apellido materno del prestatario
+    Email_borrower = models.CharField() #Direccion de correo electronico del prestatario
+    Password_borrower = models.CharField(max_length=60) #Contraseña de la cuenta del prestatario
+    Phonenumber_borrower = models.IntegerField(max_length=10) #Numero de telefono del prestatario
+    RFC_borrower = models.CharField(max_length=13) #Registro Federal de Contribuyentes (RFC) relacionado al prestatario
+    Fulladdress_borrower = models.CharField() #Direccion completa del prestatario
+    City_borrower = models.CharField() #Ciudad del prestatario
+    neighboorn_borrower = models.CharField() #Vecindario, barrio, colonia o fraccionamiento del prestatario
+    CP_borrower = models.IntegerField(max_length=5) #Codigo postal del prestatario
+    State_borrower = models.CharField() #Estado del territorio o estado del prestatario
+    Country_borrower = models.CharField() #Pais del prestatario
+    PossibilityOfPay = models.FloatField(decimal_places=2) #número que indica la posibilidad de que el prestatario cumpla con los pagos
+    ScoreLlamas = models.IntegerField() #Puntaje de calificación del prestatario dentro de la aplicación
 
-
-
-
+#Moneylender Model
+class Moneylender(models.model):
+    id_moneylender = models.CharField(primary_key=True) #Clave unica del usuario prestamista 
+    username_moneylender = models.CharField(max_length=50) #Nombre de usuario del prestamista
+    Name_moneylender = models.CharField() #Nombre completo del prestamista
+    Email_moneylender = models.CharField() #Direccion de correo electronico del prestamista
+    Password_moneylender = models.CharField(max_length=60) #Contraseña de la cuenta del prestamista
+    Subscription = models.BooleanField() #Estado que indica si el prestamista pago la suscripcion
+    LoansActive = models.IntegerField() #Prestamos que el prestatario tiene publicados/activos
 
 #Credit History Model
 class CreditHistory(models.Model):
     id_check = models.AutoField(primary_key=True)  # Clave única
-    
-    #Agregar cuando se complete el modelo de Borrower
-    #borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
-    
+    borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
     date_account_open = models.DateField()          # Fecha de apertura de la cuenta
     actual_balance = models.DecimalField(max_digits=10, decimal_places=2)  # Saldo pendiente
     max_credit = models.DecimalField(max_digits=10, decimal_places=2)      # Cantidad máxima solicitada
@@ -35,13 +64,13 @@ class CreditHistory(models.Model):
     place_of_work = models.CharField(max_length=100)  # Lugar de trabajo
     salary = models.DecimalField(max_digits=10, decimal_places=2)  # Salario
 
-
+#Request model
 class UserRequest(models.Model):
     id_request = models.AutoField(primary_key=True) #llave unica de la entidad
     Usercheck = models.ForeignKey(CreditHistory, on_delete=models.CASCADE) # llave foranea de la entidad CreditHistory,
-
+    borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
+    moneylender = models.ForeignKey(Moneylender, on_delate=models.CASCADE) #relacion con el prestamista
+    
     #estos atributos aun no estan creados
     
     #loan = models.ForeignKey(Loans, on_delete=models.CASCADE)  #relacion con la entidad loan
-    #borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)  # Relación con el prestatario
-    #moneylender = models.ForeignKey(moneylender, on_delate=models.CASCADE) #relacion con el prestamista
