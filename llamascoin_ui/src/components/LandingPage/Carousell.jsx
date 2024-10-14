d2eda12a23f2e7fcd108eea21ed1fec22fc9c2c4
Ghana001 <U21210757@utp.edu.pe>
2024-10-13 20:26:10 -0500

    images carrusel
    cambio1 imagenes carrusel

--- llamascoin_ui/src/components/LandingPage/Carousell.jsx
+++ llamascoin_ui/src/components/LandingPage/Carousell.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from "@material-tailwind/react";
 import imagen1 from '../../assets/Images_Carrusel/1.jpg'
 import imagen2 from '../../assets/Images_Carrusel/2.jpg'
+import imagen3 from '../../assets/Images_Carrusel/3.jpg'
 export function CarouselDefault() {
   return (
     <Carousel className="rounded-xl">
@@ -15,7 +16,7 @@ export function CarouselDefault() {
         className="h-full w-full max-h-[460px] object-cover"
       />
       <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+        src={imagen3}
         alt="image 3"
         className="h-full w-full max-h-[460px] object-cover"
       />
