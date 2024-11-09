ffd4439266ffbaa7b1aa4df75b862619a08bf25f
Ghana001 <U21210757@utp.edu.pe>
2024-11-08 22:35:40 -0500

    plantilla de bienvenida
    Se mejoró y agregó diseño a la plantilla de bienvenida permitiendole redireccionar al login y la pagina principal.

--- backend/services/Correos/send_mailp.py
+++ backend/services/Correos/send_mailp.py
@@ -10,7 +10,8 @@ sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'llamascoin.settings')
 django.setup()
 
-from send_mail import EmailSender  # Importa la clase EmailSender
+# Importa la clase EmailSender desde la ubicación correcta
+from send_mail import EmailSender
 
 # Solicita los datos de forma dinámica
 destinatario = input("Ingrese el correo del destinatario: ")
@@ -18,12 +19,14 @@ asunto = input("Ingrese el asunto del correo: ")
 template_name = input("Ingrese la plantilla a utilizar: ")
 nombre = input("Ingrese el nombre del destinatario: ")
 
-# Instancia y usa la clase EmailSender
+# Instancia y usa la clase EmailSender con el contexto necesario
 email_sender = EmailSender(
     recipient=destinatario,
     subject=asunto,
     template_name=template_name,
-    name=nombre
+    context={
+        'nombre': nombre
+        }  # Agrega el contexto
 )
 
 # Llama al método para enviar el correo
